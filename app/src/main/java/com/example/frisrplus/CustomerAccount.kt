ffc3a7adbf4f041f7413e5b3b9051d3f11371a50
youssefazroun <yhma22.azroyo@folkuniversitetet.nu>
2023-12-11 13:45:47 +0100

    Activities Creation.

---
+++ app/src/main/java/com/example/frisrplus/CustomerAccount.kt
@@ -0,0 +1,11 @@
+package com.example.frisrplus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomerAccount : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_account)
+    }
+}
