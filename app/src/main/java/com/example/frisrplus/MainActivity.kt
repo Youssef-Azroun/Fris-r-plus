b2b52108daf8c8b2e34860370e9e4fa9f72c227e
Kanyaton Somjit <113116925+Kanyatonsita@users.noreply.github.com>
2023-12-12 16:34:41 +0100

    firstDesigMainActivity

--- app/src/main/java/com/example/frisrplus/MainActivity.kt
+++ app/src/main/java/com/example/frisrplus/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.frisrplus
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
+        val adressTextView: TextView = findViewById(R.id.adressTextView)
+        adressTextView.text =
+            "Carl Krooks Gata 6, \n" + "252 25 Helsingborg"
+        val mailTextView: TextView = findViewById(R.id.mailTextView)
+        mailTextView.text =
+            "frisorplus.admin@gmail.com"
 
 
         val testButton: Button = findViewById(R.id.buttonToSignIn)
