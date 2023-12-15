074e239c8474d552ae3332ac57c5ce7629c2e0cd
Kanyaton Somjit <113116925+Kanyatonsita@users.noreply.github.com>
2023-12-15 17:16:15 +0100

    CreateMoreAktivite

---
+++ app/src/main/java/com/example/frisrplus/DayAndTimeForBookingAvailableActivity.kt
@@ -0,0 +1,11 @@
+package com.example.frisrplus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DayAndTimeForBookingAvailableActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_day_and_time_for_booking_available)
+    }
+}
