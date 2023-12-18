90b9994e0e31170317014a551b51858ef5f2cf78
youssefazroun <yhma22.azroyo@folkuniversitetet.nu>
2023-12-18 16:36:04 +0100

    First design for calendar and time

--- app/src/main/java/com/example/frisrplus/ServicesRecycleAdapter.kt
+++ app/src/main/java/com/example/frisrplus/ServicesRecycleAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.frisrplus
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -26,10 +28,20 @@ class ServicesRecycleAdapter (val context: Context, val services : List<Services
 
         holder.servicedNameTextView.text = services.servicedName
         holder.priceTextView.text = services.price
+
+        // Set OnClickListener for the "Boka" button
+        holder.bookingButton.setOnClickListener {
+            val context = holder.itemView.context
+            val intent = Intent(context, DayAndTimeForBookingAvailableActivity::class.java)
+            // You may want to pass some data to DayAndTimeForBookingAvailableActivity using intent.putExtra if needed
+            context.startActivity(intent)
+        }
     }
+
 
     inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
         var servicedNameTextView = itemView.findViewById<TextView>(R.id.servicedNameTextView)
         var priceTextView = itemView.findViewById<TextView>(R.id.priceTextView)
+        var bookingButton = itemView.findViewById<Button>(R.id.bookingButton)
     }
 }
