469920d988541e656ab1e26820f7977d0e66721e
youssefazroun <yhma22.azroyo@folkuniversitetet.nu>
2023-12-18 18:41:32 +0100

    Bookings for logged in user

--- app/src/main/java/com/example/frisrplus/BookingInfoCustomerLoggedActivity.kt
+++ app/src/main/java/com/example/frisrplus/BookingInfoCustomerLoggedActivity.kt
@@ -1,11 +1,168 @@
 package com.example.frisrplus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
 
 class BookingInfoCustomerLoggedActivity : AppCompatActivity() {
+
+    private lateinit var firestore: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
+
+    // Declare TextView variables here
+    private lateinit var userEmailTextView: TextView
+    private lateinit var firstNameTextView: TextView
+    private lateinit var lastNameTextView: TextView
+    private lateinit var phoneNumberTextView: TextView
+    private lateinit var selectedDateTextView: TextView
+    private lateinit var bokaButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_booking_info_customer_logged)
+
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+        // Get the information from the intent
+        val typeOfCut: String? = intent.getStringExtra("typeOfCut")
+        val price: String? = intent.getStringExtra("price")
+        val selectedTime: String? = intent.getStringExtra("selectedTime")
+
+        // Initialize TextView variables
+        userEmailTextView = findViewById(R.id.userEmailTextView)
+        firstNameTextView = findViewById(R.id.firstNameTextView)
+        lastNameTextView = findViewById(R.id.lastNameTextView)
+        phoneNumberTextView = findViewById(R.id.phoneNumberTextView)
+        selectedDateTextView = findViewById(R.id.selectedDateTextView)
+
+        // Update your UI elements with the information
+        val typeOfCutTextView: TextView = findViewById(R.id.typeOfCutTextView)
+        val priceTextView: TextView = findViewById(R.id.priceTextView)
+        val selectedTimeTextView: TextView = findViewById(R.id.selectedTimeTextView)
+
+        typeOfCutTextView.text = typeOfCut
+        priceTextView.text = price
+        selectedTimeTextView.text = selectedTime
+
+        val selectedDate: Long = intent.getLongExtra("selectedDate", 0L)
+        selectedDateTextView.text = "Selected Date: ${formatDate(selectedDate)}"
+
+        bokaButton = findViewById(R.id.bokaButton)
+
+        // Set OnClickListener for the "Boka" button
+        bokaButton.setOnClickListener {
+            // Handle the "Boka" button click
+            saveBookingToFirestore()
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+        // Fetch and display user information from Firestore
+        fetchAndDisplayUserInfo()
+    }
+
+    private fun formatDate(dateInMillis: Long): String {
+        // Implement your logic to format the date as needed
+        // For example, you can use SimpleDateFormat
+        // For demonstration purposes, we'll use a basic format here
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        return dateFormat.format(Date(dateInMillis))
+    }
+
+    private fun saveBookingToFirestore() {
+        val currentUser = auth.currentUser
+
+        if (currentUser != null) {
+            val uid = currentUser.uid
+            val typeOfCut: String? = intent.getStringExtra("typeOfCut")
+            val price: String? = intent.getStringExtra("price")
+            val selectedTime: String? = intent.getStringExtra("selectedTime")
+            val selectedDateMillis: Long = intent.getLongExtra("selectedDate", 0L)
+
+            // Convert the selectedDateMillis to a formatted date string
+            val selectedDateString = formatDate(selectedDateMillis)
+
+            // Fetch user information from Firestore
+            firestore.collection("Users")
+                .document(uid)
+                .get()
+                .addOnSuccessListener { documentSnapshot ->
+                    if (documentSnapshot.exists()) {
+                        val user = documentSnapshot.toObject(User::class.java)
+                        if (user != null) {
+                            // Create a data map with both user and booking information
+                            val bookingData = hashMapOf(
+                                "typeOfCut" to typeOfCut,
+                                "price" to price,
+                                "selectedTime" to selectedTime,
+                                "selectedDate" to selectedDateString, // Use the formatted date string
+                                "email" to user.email,
+                                "firstName" to user.firstName,
+                                "lastName" to user.lastName,
+                                "phoneNumber" to user.phoneNumber
+                            )
+
+                            // Save the booking data to Firestore
+                            firestore.collection("Bookings")
+                                .document(uid)
+                                .collection("UserBookings")
+                                .add(bookingData) // Use add instead of set
+                                .addOnSuccessListener {
+                                    showToast("Booking information saved successfully")
+                                }
+                                .addOnFailureListener { e ->
+                                    showToast("Error saving booking information: ${e.message}")
+                                }
+                        }
+                    }
+                }
+                .addOnFailureListener { e ->
+                    showToast("Error fetching user information: ${e.message}")
+                }
+        }
+    }
+
+
+
+    private fun fetchAndDisplayUserInfo() {
+        val currentUser = auth.currentUser
+
+        if (currentUser != null) {
+            val uid = currentUser.uid
+
+            firestore.collection("Users")
+                .document(uid)
+                .get()
+                .addOnSuccessListener { documentSnapshot ->
+                    if (documentSnapshot.exists()) {
+                        val user = documentSnapshot.toObject(User::class.java)
+                        if (user != null) {
+                            userEmailTextView.text = "Email: ${user.email}"
+                            firstNameTextView.text = "First Name: ${user.firstName}"
+                            lastNameTextView.text = "Last Name: ${user.lastName}"
+                            phoneNumberTextView.text = "Phone Number: ${user.phoneNumber}"
+                        }
+                    }
+                }
+                .addOnFailureListener { e ->
+                    showToast("Error fetching user information: ${e.message}")
+                }
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
+
+
--- app/src/main/java/com/example/frisrplus/DayAndTimeForBookingAvailableActivity.kt
+++ app/src/main/java/com/example/frisrplus/DayAndTimeForBookingAvailableActivity.kt
@@ -1,22 +1,61 @@
 package com.example.frisrplus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.CalendarView
+import android.widget.LinearLayout
+import com.google.firebase.auth.FirebaseAuth
 
-class  DayAndTimeForBookingAvailableActivity: AppCompatActivity() {
+class DayAndTimeForBookingAvailableActivity : AppCompatActivity() {
+    private var typeOfCut: String? = null
+    private var price: String? = null
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_day_and_time_for_booking_available)
 
-        // Find the CalendarView in your layout
+        auth = FirebaseAuth.getInstance()
+
+        typeOfCut = intent.getStringExtra("typeOfCut")
+        price = intent.getStringExtra("price")
+
         val calendarView: CalendarView = findViewById(R.id.calendarView)
 
-        // Set a listener for date changes
-        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
-            // Handle the selected date
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
             val selectedDate = "$year-${month + 1}-$dayOfMonth"
-            // Implement your logic here
+            navigateToBookingInfoActivity(selectedDate)
         }
+
+        val buttonContainer: LinearLayout = findViewById(R.id.buttonContainer)
+        for (i in 0 until buttonContainer.childCount) {
+            val button: Button = buttonContainer.getChildAt(i) as Button
+            button.setOnClickListener {
+                navigateToBookingInfoActivity(button.text.toString())
+            }
+        }
+    }
+
+    private fun navigateToBookingInfoActivity(selectedTime: String) {
+        val intent: Intent
+        if (auth.currentUser != null) {
+            // User is logged in, navigate to BookingInfoCustomerLoggedActivity
+            intent = Intent(this, BookingInfoCustomerLoggedActivity::class.java)
+        } else {
+            // User is not logged in, navigate to BookingInfoForCostumerNoAccountsActivity
+            intent = Intent(this, BookingInfoForCostumerNoAccountsActivity::class.java)
+        }
+
+        intent.putExtra("typeOfCut", typeOfCut)
+        intent.putExtra("price", price)
+        intent.putExtra("selectedTime", selectedTime)
+
+        // Pass the selected date to BookingInfoCustomerLoggedActivity
+        val calendarView: CalendarView = findViewById(R.id.calendarView)
+        intent.putExtra("selectedDate", calendarView.date)
+
+        startActivity(intent)
     }
 }
