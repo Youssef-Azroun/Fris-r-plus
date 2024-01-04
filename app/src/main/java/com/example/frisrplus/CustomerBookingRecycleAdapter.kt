034363ae3b04ce966f5c89dbb35f149d8b8ea110
youssefazroun <yhma22.azroyo@folkuniversitetet.nu>
2024-01-04 14:57:30 +0100

    avboka and tabort button for user

--- app/src/main/java/com/example/frisrplus/CustomerBookingRecycleAdapter.kt
+++ app/src/main/java/com/example/frisrplus/CustomerBookingRecycleAdapter.kt
@@ -1,12 +1,15 @@
 package com.example.frisrplus
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 interface ItemClickListener {
     fun onItemClick(userBooking: UserBooking)
@@ -29,11 +32,12 @@ class CustomerBookingRecycleAdapter(private val context: Context,
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val userBooking = userBookings[position]
 
-        // Anpassa knappens text och beteende baserat på användarens roll
+        // Set the button text to "Ta bort" for the admin
         if (isAdmin) {
             holder.cancelButton.text = "Ta bort"
         } else {
-            holder.cancelButton.text = "Avboka"
+            // Check if the booking exists in AllBookings for non-admin users
+            checkBookingExistsInAllBookings(userBooking.bookingId, holder)
         }
 
         // Klickhanterare för knappen
@@ -43,9 +47,33 @@ class CustomerBookingRecycleAdapter(private val context: Context,
 
         // Populate your ViewHolder views with data from userBooking
         holder.custumerBookingInfoTextView.text = "Name: ${userBooking.firstName} ${userBooking.lastName}\nE-post: ${userBooking.email}\n" +
-                                    "Tel: 0${userBooking.phoneNumber.toString()}\nPris: ${userBooking.price.toString()}" +
-                                     "\nDatum: ${userBooking.selectedDate}\nTid: ${userBooking.selectedTime}\nTyp: ${userBooking.typeOfCut}"
+                "Tel: 0${userBooking.phoneNumber.toString()}\nPris: ${userBooking.price.toString()}" +
+                "\nDatum: ${userBooking.selectedDate}\nTid: ${userBooking.selectedTime}\nTyp: ${userBooking.typeOfCut}"
     }
+
+
+    private fun checkBookingExistsInAllBookings(bookingId: String?, holder: ViewHolder) {
+        if (bookingId != null) {
+            val allBookingsRef = Firebase.firestore.collection("AllBookings").document(bookingId)
+
+            allBookingsRef.get()
+                .addOnSuccessListener { documentSnapshot ->
+                    if (documentSnapshot.exists()) {
+                        // Booking exists in AllBookings, set button text to "Avboka"
+                        holder.cancelButton.text = "Avboka"
+                    } else {
+                        // Booking doesn't exist in AllBookings, set button text to "Ta bort"
+                        holder.cancelButton.text = "Ta bort"
+                    }
+                }
+                .addOnFailureListener { e ->
+                    Log.e("CheckBookingExists", "Error checking booking in AllBookings: $e")
+                }
+        } else {
+            Log.e("CheckBookingExists", "Invalid bookingId")
+        }
+    }
+
 
     override fun getItemCount(): Int {
         return userBookings.size
